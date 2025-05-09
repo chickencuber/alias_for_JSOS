b110c815bca54c3f7e3646bde5acb2e3afd54302
chickencuber <n1batman728@gmail.com>
2025-05-09 16:28:49 -0500

    made it even better

--- cmd.exe.js
+++ cmd.exe.js
@@ -1,7 +1,16 @@
-if(!args[0] || !args[1]) return "expected at least 2 arguments"
-const n = `/cmd/${args.shift()}`;
+if (flags.includes("r")) {
+    if(!arg[0]) return "expected an argument";
+    const n = `/cmd/${args[0]}.sh`;
+    if(! await FS.exists(n)) {
+        return "path doesn't exist"
+    }
+    await FS.delete(n);
+} else {
+    if(!args[0] || !args[1]) return "expected at least 2 arguments"
+    const n = `/cmd/${args.shift()}`;
 
-if(await FS.exists(n+".sh") || await FS.exists(n + ".exe")) {
-  return "path already exists";
+    if(await FS.exists(n+".sh") || await FS.exists(n + ".exe")) {
+        return "path already exists";
+    }
+    await FS.addFile(n + ".sh", args.join(" "));
 }
-await FS.addFile(n + ".sh", args.join(" "));
